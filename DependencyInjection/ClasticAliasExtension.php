cdf164ab67387d9298498bc6931838b62e253d1d
Dries De Peuter <dries@nousefreak.be>
2015-01-04 21:52:28 +0100

    [Node] Extract node logic.

--- DependencyInjection/ClasticAliasExtension.php
+++ DependencyInjection/ClasticAliasExtension.php
@@ -11,7 +11,6 @@ namespace Clastic\AliasBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
